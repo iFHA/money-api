a27c489ad6fbf621c602bf4c630bddf96dc4d10e
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2024-02-24 12:16:08 -0300

    migration de lancamento

---
+++ src/main/resources/db/migration/V3__create-table-lancamento.sql
@@ -0,0 +1,13 @@
+CREATE TABLE lancamento (
+    id bigint(20) auto_increment primary key,
+    descricao varchar(50) not null,
+    data_vencimento DATE NOT NULL,
+    data_pagamento DATE,
+    valor decimal(15,2) NOT NULL,
+    observacao VARCHAR(20) NOT NULL,
+    tipo VARCHAR(20) NOT NULL,
+    categoria_id BIGINT(20) NOT NULL,
+    pessoa_id BIGINT(20) NOT NULL,
+    FOREIGN KEY (categoria_id) REFERENCES categoria(id),
+    FOREIGN KEY (pessoa_id) REFERENCES pessoa(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
